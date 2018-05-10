5d9e236bd7ebeeb3ac176704358aaa54fabaff7f
Tristan Paust <tristanpaust@gmail.com>
2018-05-09 23:34:55 -0700

    identifying number types

--- parseJSON.hs
+++ parseJSON.hs
@@ -8,12 +8,15 @@ type Name = String -- This is sort of the label of the object / sub-object
 data JSONData = JSONData Name JSON -- A JSON object has a label (Name) which is followed by colon + curly braces and data
   deriving Show
 
-data JSON = Object [JSONData] -- The whole object, as well as sub-objects
+data Numbers = Fraction Float | Integer Int | Negative Int | Exponential String
+  deriving Show
+
+data JSON = Object [JSONData] -- One whole object, as well as potential sub-objects
           | Boolean Bool 
           | Str String 
-          | Number Double
+          | Number Numbers
           | Array [JSON] -- JSON Arrays repeat any of the other types / itself
-          | None -- In case there is an empty node in the file
+          | None -- In case there is a nullpointer in the file
   deriving Show
 
 json :: Parser JSON
@@ -24,7 +27,7 @@ object = do
     spaces
     char '{'
     spaces
-    kind <- parseJSONData `sepBy` (spaces >> char ',' >> spaces)-- This gives the entire line, e.g. "test-attribute" : value
+    kind <- parseJSONData `sepBy` (spaces >> char ',' >> spaces) -- kind is the entire line, e.g. "test-attribute" : value,
     spaces
     char '}'
     spaces
@@ -34,12 +37,14 @@ object = do
 parseJSONData :: Parser JSONData
 parseJSONData = do 
     name <- getName
-    value <- (boolean
+    value <- 
+         (    object
+         <|>  array 
+         <|>  boolean 
+         <|>  none
          <|>  str
          <|>  number
-         <|>  array
-         <|>  none
-         <|>  object)
+         )
     return (JSONData name value)
 
 getName :: Parser Name
@@ -72,19 +77,35 @@ parseStr = between (char '\"') (char '\"') (many $ noneOf "\"" <|> try (string "
 number :: Parser JSON
 number = do
     spaces
-    fullNumber <- many (digit <|> oneOf ".-") -- Number can be int, which is only digits, or a floating point number of a negative number
-    return (Number (read fullNumber))
+    fullNumber <- many (digit <|> oneOf ".-+eE") -- Number can be int, which is only digits, or a floating point number of a negative number
+    spaces
+    if (charFound (head "e") fullNumber) then return (Number (Exponential (fullNumber))) -- Note: I am only returning exponentials as strings
+    else if (charFound (head "E") fullNumber) then return (Number (Exponential (fullNumber)))
+    else if (charFound (head ".") fullNumber) && (charFound (head "-") fullNumber) then return (Number (Fraction (read fullNumber))) -- Negative float
+    else if (charFound (head ".") fullNumber) then return (Number (Fraction (read fullNumber)))
+    else if (charFound (head "-") fullNumber) then return (Number (Negative (read fullNumber)))
+    else return (Number (Integer (read fullNumber)))
+
+charFound :: Char -> String -> Bool
+charFound _ [] = False 
+charFound c (x:xs)
+    | c == x = True
+    | otherwise = charFound c xs
 
 array :: Parser JSON
 array = do
     spaces
     char '['
-    arr <- sepBy (object
+    spaces
+    arr <-  
+        (   object
         <|> array
         <|> boolean
         <|> none
         <|> str
-        <|> number) (spaces >> char ',' >> spaces)
+        <|> number
+        ) 
+        `sepBy` (spaces >> char ',' >> spaces)
     spaces 
     char ']'
     spaces
@@ -97,6 +118,10 @@ none = do
     spaces
     return None
 
-runTest = do 
-    test <- readFile "test.json"
+runTest1 = do 
+    test <- readFile "test1.json"
+    parseTest json test
+
+runTest2 = do
+    test <- readFile "test2.json"
     parseTest json test
